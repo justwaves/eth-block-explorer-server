7773c4e1d1a297f3944adc70b6eb7ffee5f07ac5
uijin <justwaves@naver.com>
2020-06-12 18:31:33 +0900

    build setting

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 
@@ -13,6 +16,7 @@ const router = new Router();
 const corsOptions = {
   origin: 'https://eth-block-explorer.netlify.app',
   optionsSuccessStatus: 200,
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -23,6 +27,17 @@ app.use(bodyParser());
 
 // app 인스턴스에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(
+  __dirname,
+  '../../eth-block-explorer/build',
+);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
