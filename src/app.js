c3319011f0dea8cf287ab43b97a1a7e4e6dff112
uijin <justwaves@naver.com>
2020-06-12 21:09:16 +0900

    Change

--- src/app.js
+++ src/app.js
@@ -8,14 +8,16 @@ import api from './api';
 const { PORT } = process.env;
 
 const app = new Koa();
-const router = new Router();
 
 const corsOptions = {
   origin: 'https://eth-block-explorer.netlify.app',
   optionsSuccessStatus: 200,
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+
+const router = new Router();
 
 // api route 적용
 router.use('/api', api.routes());
