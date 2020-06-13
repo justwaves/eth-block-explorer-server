9bb9136613f0dc2172318a808136eeeddc5005a4
uijin <justwaves@naver.com>
2020-06-13 16:18:03 +0900

    build

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 
@@ -26,6 +29,17 @@ app.use(bodyParser());
 
 // app 인스턴스에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(
+  __dirname,
+  '../../eth-block-explorer/build',
+);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
